1dde66d907d533c6202c8f3ac6a8b03d72bf6149
resumbacher <marco@hellwiedunkel.de>
2025-07-28 20:28:47 +0200

    Update streamlit_app.py
    url rechange

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        #smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/all" + fruit_chosen)
-        smoothiefroot_response=requests.get("https://my.smoothiefront.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/all" + fruit_chosen)
+        #smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 #alter table SMOOTHIES.PUBLIC.ORDERS add column name_on_order varchar(100);
