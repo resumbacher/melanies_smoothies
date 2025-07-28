8a37ad9129dc7f2e8371b93eb02c4a1cb649b083
resumbacher <marco@hellwiedunkel.de>
2025-07-28 18:11:14 +0200

    Update streamlit_app.py
    added request

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+#st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = true)
