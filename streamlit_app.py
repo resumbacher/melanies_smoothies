557fa2cf14b72e876e295f7e5912051ddbe5ef6b
resumbacher <marco@hellwiedunkel.de>
2025-07-28 20:38:29 +0200

    Update streamlit_app.py
    adding iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df=my_dataframe.to_pandas()
+st.data(dataframe(pd_df)
+st.stop()
+
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -37,6 +41,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', searchon, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response=requests.get("https://fruityvice.com/api/fruit/all" + fruit_chosen)
         #smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
